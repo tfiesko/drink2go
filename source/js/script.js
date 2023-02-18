97d0201749e6b5f7c974dfd05b3a5a4580ed8d5c
Anton Tarasenko <report641@gmail.com>
2023-02-19 03:28:12 +0700

    Завершение стилизации шапки

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,16 @@
-/* в этот файл добавляет скрипты*/
+let mainNav = document.querySelector('.main-nav');
+let mainNavToggle = mainNav.querySelector('.main-nav__toggle');
+
+mainNav.classList.remove('main-nav--nojs');
+
+mainNavToggle.addEventListener('click', ()=> {
+  if(mainNav.classList.contains('main-nav--close')) {
+    mainNav.classList.remove('main-nav--close');
+    mainNav.classList.add('main-nav--open');
+  }
+
+  else {
+    mainNav.classList.remove('main-nav--open');
+    mainNav.classList.add('main-nav--close');
+  }
+});
