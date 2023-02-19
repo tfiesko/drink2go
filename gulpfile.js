a7b52a708294c0d3148bfc5178c86c2fecbfc606
Anton Tarasenko <report641@gmail.com>
2023-02-20 04:01:52 +0700

    Добавлена карта

--- gulpfile.js
+++ gulpfile.js
@@ -85,6 +85,7 @@ export function copyAssets () {
     'source/fonts/**/*.{woff2,woff}',
     'source/*.ico',
     'source/*.webmanifest',
+    'source/vendor/**/*.*',
   ], {
     base: 'source'
   })
--- source/js/script.js
+++ source/js/script.js
@@ -14,3 +14,34 @@ mainNavToggle.addEventListener('click', ()=> {
     mainNav.classList.add('main-nav--close');
   }
 });
+
+const map = L.map('map')
+  .setView({
+    lat: 59.96831,
+    lng: 30.31748,
+  }, 18);
+
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  },
+).addTo(map);
+
+const customIcon = L.icon({
+  iconUrl: './img/Map-Pin.png',
+  iconSize: [38, 50],
+  iconAnchor: [19, 50],
+});
+
+const marker = L.marker(
+  {
+    lat: 59.96831,
+    lng: 30.31748,
+  },
+  {
+    icon: customIcon,
+  }
+);
+
+marker.addTo(map);
